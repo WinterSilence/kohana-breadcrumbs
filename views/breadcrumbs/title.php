c56a18065f3ffbceb7aefae80948b565287b6602
WinterSilence <info@handy-soft.ru>
2013-09-13 07:49:41 +0400

    fix

--- views/breadcrumbs/title.php
+++ views/breadcrumbs/title.php
@@ -1,4 +1,4 @@
-<?php defined('SYSPATH') OR die('No direct script access.') />
+<?php defined('SYSPATH') OR die('No direct script access.') ?>
 
 <?php if (isset($breadcrumbs)): ?>
 	<?php foreach ($breadcrumbs as $url => $name): ?>
