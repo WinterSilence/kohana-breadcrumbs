1feb36bba698706ca4e6af52dc8dc9f30b03d62a
WinterSilence <info@handy-soft.ru>
2013-09-13 10:31:01 +0400

    update views

--- views/breadcrumbs/default.php
+++ views/breadcrumbs/default.php
@@ -1,18 +1,14 @@
 <?php defined('SYSPATH') OR die('No direct script access.') ?>
 
-<?php if (isset($breadcrumbs) AND $total > 1): ?>
-
+<?php if ($total > 1): ?>
 	<ul class="f-breadcrumbs">
 	<?php $cnt = 1 ?>
 	<?php foreach ($breadcrumbs as $url => $name): ?>
-
 		<?php if ($cnt++ < $total): ?>
 		<li><?php echo HTML::anchor($url, __(UTF8::ucfirst($name))) ?></li>
 		<?php else: ?>
 		<li class="cur"><?php echo __(UTF8::ucfirst($name)) ?></li> 
 		<?php endif ?>
-
 	<?php endforeach ?>
 	</ul>
-
 <?php endif ?>
--- views/breadcrumbs/title.php
+++ views/breadcrumbs/title.php
@@ -1,9 +1,5 @@
 <?php defined('SYSPATH') OR die('No direct script access.') ?>
 
-<?php if (isset($breadcrumbs)): ?>
-	<?php foreach ($breadcrumbs as $url => $name): ?>
-		<?php $breadcrumbs[$url] = __(UTF8::ucfirst($name) ?>
-	<?php endforeach ?>
-	<?php if ( ! isset($separator)) $separator = ' - ' ?>
-	<?php echo implode($separator, $breadcrumbs) ?>
-<?php endif ?>
+<?php foreach ($breadcrumbs as $url => $name) $breadcrumbs[$url] = __(UTF8::ucfirst($name)) ?>
+<?php if ( ! isset($separator) $separator = ' - ' ?>
+<title><?php echo implode($separator, $breadcrumbs) ?></title>
